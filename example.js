8925a9497d3bc347d767f41455e5e0318d004657
Swapna Dasari <147240307+swapna431@users.noreply.github.com>
2024-12-13 16:11:18 +0530

    Add files via upload

---
+++ example.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Example2()
+{
+    return <h1>Ford.....</h1>
+    function Example(){
+        return <div>
+            <Example2></Example2>
+            <h1> I am......</h1>
+            const x=10
+        </div>
+    }
+}
